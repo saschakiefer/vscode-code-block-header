58f3b6f577be5a5e008c9319c974e07c32524842
Sascha Kiefer <mail@sascha-kiefer.de>
2020-04-17 10:09:53 +0200

    First functional release

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,22 +9,62 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  // console.log(
+  //   'Congratulations, your extension "vscode-code-block-header" is now active!'
+  // );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-code-block-header" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "vscode-code-block-header.blockHeader",
+    function () {
+      let editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-code-block-header.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      if (editor) {
+        let document = editor.document;
+        let selection = editor.selection;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-code-block-header!');
-	});
+        // Only works for single lines
+        if (!selection.isSingleLine) {
+          return;
+        }
 
-	context.subscriptions.push(disposable);
+        let line = selection.active.line;
+        let text = document.lineAt(line).text;
+
+        let length = 72 - text.length - 2 - 2; // -2 Spaces - '# '
+        let quotient = Math.floor(length / 2);
+        let remainder = length % 2;
+
+        let block =
+          "# " +
+          "-".repeat(quotient) +
+          " " +
+          text +
+          " " +
+          "-".repeat(quotient) +
+          "-".repeat(remainder);
+
+        editor
+          .edit((editBuilder) => {
+            editBuilder.replace(
+              new vscode.Range(line, 0, line, text.length),
+              block
+            );
+          })
+          .then((success) => {
+            console.log("success:", success);
+            var position = new vscode.Position(line, block.length);
+            editor.selection = new vscode.Selection(position, position);
+          });
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -32,6 +72,6 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
